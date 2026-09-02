790727e6ddad808d36c3b692841fdce6884ca51a
Niteesh kumar <niteeshrajput131@gmail.com>
2026-09-03 02:43:15 +0530

    Refactor union function to use rank for optimization
    Updated union function to include rank for union by rank optimization.

--- graph/Disjoint Set Union Find.cpp
+++ graph/Disjoint Set Union Find.cpp
@@ -4,11 +4,21 @@ int find(int i,vector<int>&parent){
     return parent[i] = find(parent[i],parent);
 }
 
-void union(int x, int y, vector<int>&parent){
+void union(int x, int y, vector<int>&parent,vector<int>&rank){
     int x_par= find(x,parent);
     int y_par= find(y,parent);
 
-    if(x_par != y_par){
+    if(x_par == y_par){
+        return;
+    }
+    else if(rank[x_par] > rank[y_par]){
+        parent[y_par] = x_par;
+    }
+    else if(rank[y_par] > rank[x_par]){
         parent[x_par] = y_par;
+    }
+    else{
+        parent[x_par] = y_par;
+        rank[y_par] += 1;
     }
 }
