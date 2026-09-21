f4093e340fa4d537079242a861cfc644a2456c58
Niteesh kumar <niteeshrajput131@gmail.com>
2026-09-22 03:50:00 +0530

    Add right side view function for binary tree
    Implemented a solution to return the right side view of a binary tree using BFS.

---
+++ Tree/Binary Tree Right Side View.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        
+        if(root == NULL) return {};
+
+        // bfs use karo  or window (ik level ka ) ka last vala node ans me push karo.
+        vector<int>ans;
+
+        queue<TreeNode*>q;
+        q.push(root);
+        int cnt =0;
+        while(!q.empty()){
+
+            int sz = q.size();
+            while(sz--){
+                auto node = q.front();
+    
+                q.pop();
+
+                if(sz == 0){
+                    ans.push_back(node->val);  // last node of the level.
+                }
+                if(node->left){
+                    q.push(node->left);
+                }
+
+                if(node->right){
+                    q.push(node->right);
+                }
+            }
+        }
+        return ans;    
+    }
+};
