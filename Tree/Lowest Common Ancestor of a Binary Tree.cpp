a97348d8a600a4ca663072cc86dfaca4d67aaf8c
Niteesh kumar <niteeshrajput131@gmail.com>
2026-09-22 23:58:26 +0530

    Implement second method for lowest common ancestor
    Added a second method to find the lowest common ancestor using path tracing.

--- Tree/Lowest Common Ancestor of a Binary Tree.cpp
+++ Tree/Lowest Common Ancestor of a Binary Tree.cpp
@@ -7,7 +7,7 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-class Solution {
+class Solution {   // METHOD - 1
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(root == nullptr || root == p || root == q) return root;
@@ -18,5 +18,48 @@ public:
         if(left && right) return root;  // if both left and right are present then root will always be LCA
 
         return right ? right : left; // if either be present left or right
+    }
+};
+
+
+// METHOD - 2;
+
+class Solution {
+public:
+bool findPath(TreeNode* root, TreeNode* target, vector<TreeNode*>& path) {
+        if(root == nullptr)
+            return false;
+
+        path.push_back(root);
+
+        if(root == target)
+            return true;
+
+        if(findPath(root->left, target, path) ||
+           findPath(root->right, target, path))
+            return true;
+
+        path.pop_back();
+        return false;
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        vector<TreeNode*> pathP;
+        vector<TreeNode*> pathQ;
+
+        findPath(root, p, pathP);  // FIND THE ANCESTORS OF P 
+        findPath(root, q, pathQ); //  FIND THE ANCESTORS OF Q
+
+        TreeNode* ans = nullptr;
+
+        int i = 0; // FIND THE FIRST COMMON ANCESTOR , AND OUR LCA AS WELL . 
+        while(i < pathP.size() && i < pathQ.size()) {
+            if(pathP[i] != pathQ[i])  
+                break;
+
+            ans = pathP[i];
+            i++;
+        }
+
+        return ans;
     }
 };
